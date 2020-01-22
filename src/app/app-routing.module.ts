03c8254396a7943e9325572d4cc839e9a063f057
Zagade Kaustubh <pranjan.bhure14@vit.edu>
2020-01-22 21:46:29 +0530

    New routes for bill and order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './welcome/login/login.component';
 import { RegisterComponent } from './welcome/register/register.component';
+import { BillComponent } from '../app/customer/bill/bill.component';
+import { MenuSelectionComponent } from './customer/menu.selection/menu.selection.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'bill', component: BillComponent },
+  { path: 'modify', component: MenuSelectionComponent },
 ];
 
 @NgModule({
