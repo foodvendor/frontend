fe43be5e6fb059ec05ba78d1b2e2ea7bec4c3913
Zagade Kaustubh <pranjan.bhure14@vit.edu>
2020-01-22 13:10:58 +0530

    Miscellaneous files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './welcome/login/login.component';
+import { RegisterComponent } from './welcome/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
