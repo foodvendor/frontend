28abd3e97cd35d24ce7abf35865de5de20c382f5
Zagade Kaustubh <pranjan.bhure14@vit.edu>
2020-01-22 13:09:11 +0530

    Register Component

---
+++ src/app/welcome/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    address: new FormGroup({
+      add1: new FormControl(''),
+      add2: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zipcode: new FormControl(''),
+      country: new FormControl(''),
+    })
+  });
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
